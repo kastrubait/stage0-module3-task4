958db3f6b8e8ca8d0103d3eec72991424d3ea269
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-08 10:49:44 +0300

    feat: complete tast4 module3 v.2

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.module3.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
         // double res;
-        int a, b, c;
+        int a=1, b=1, c=1;
         //int pres1 = 9 * a * a - 5 * b + a - 7;
         //double pres2 = (a + b - 4 * a * c) >> 1;
         double res = (9 * a * a - 5 * b + a - 7) * ((a + b - 4 * a * c) / 2);
