eb809485f5c7bb7f1c3853b3c266a0e4408cab05
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-08 10:41:06 +0300

    feat: complete tast4 module3 v.0

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double res;
+        int a=1, b=1, c=1;
+        int pres1 = 9 * a * a - 5 * b + a - 7;
+        double pres2 = (a + b - 4 * a * c) >> 1;
+        res = pres1 * pres2;
+        System.out.println(res);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int code = 99;
+        char c = (char) code;
+        System.out.println(c);
+
+        code = 104;
+        char h = (char) code;
+        System.out.println(h);
+
+        code = 97;
+        char a = (char) code;
+        System.out.println(a);
+
+        code = 114;
+        char r = (char) code;
+        System.out.println(r);
     }
 }
